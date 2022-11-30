a0b77f01997c0ae0e5cd2dc320f7c5d2965aa645
lunar moon <49crehbgr@gmail.com>
2022-11-30 13:03:27 +0900

    Time: 546 ms (83.71%), Space: 15 MB (96.80%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,12 +1,13 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         n = len(nums)
+        dp = [False] * n
+        dp[0] = nums[0]
         
-        step = 0
-        for i in range(n):
-            if step < i:
-                break
-            step = max(step, i + nums[i])
-            if step >= n - 1:
+        for i in range(1, n):
+            if dp[i - 1] >= n - 1:
                 return True
-        return False
+            if dp[i - 1] < i:
+                return False
+            dp[i] = max(dp[i - 1], i + nums[i])
+        return dp[n - 1] >= n - 1
