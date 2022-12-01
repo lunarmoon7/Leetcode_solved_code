7046963e76d65aa164c8d5f623746beba793ef6c
lunar moon <49crehbgr@gmail.com>
2022-12-01 17:02:56 +0900

    Time: 150 ms (68.40%), Space: 13.9 MB (91.46%) - LeetHub

--- 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.py
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.py
@@ -1,24 +1,8 @@
-class Solution:
-    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
-        cnt = 0
-        row, col = len(grid), len(grid[0])
-        upDown, leftRight = [], []
-        for x in range(row):
-            leftRight.append(max(grid[x]))
-            
-        
-        for y in range(col):
-            maxs = 0
-            for x in range(row):
-                maxs = max(maxs, grid[x][y])
-            upDown.append(maxs)
-    
-        for x in range(row):
-            w = leftRight[x]
-            for y in range(col):
-                h = upDown[y]
-                cnt += min(w, h) - grid[x][y]        
-        return cnt
-    
-    
-                
+class Solution(object):
+    def maxIncreaseKeepingSkyline(self, grid):
+        row_maxes = [max(row) for row in grid]
+        col_maxes = [max(col) for col in zip(*grid)]
+
+        return sum(min(row_maxes[r], col_maxes[c]) - val
+                   for r, row in enumerate(grid)
+                   for c, val in enumerate(row))
