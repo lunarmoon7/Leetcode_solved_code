4f833736fd2b3b00d458ca006c3fe396d18726c7
lunar moon <49crehbgr@gmail.com>
2022-12-04 17:46:35 +0900

    Time: 107 ms (83.75%), Space: 13.8 MB (83.11%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -2,27 +2,29 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         n = 9
         
-        rows = [set() for _ in range(n)]
-        cols = [set() for _ in range(n)]
-        subs = [set() for _ in range(n)]
+        rows = [[0] * n for _ in range(n)]
+        cols = [[0] * n for _ in range(n)]
+        subs = [[0] * n for _ in range(n)]
         
         for x in range(n):
             for y in range(n):
-                val = board[x][y]
-                if val == ".": continue
                 
-                if val in rows[x]: 
-                    return False
-                rows[x].add(val)
+                if board[x][y] == ".": continue
                 
-                if val in cols[y]: 
+                pos = int(board[x][y]) - 1
+                
+                if rows[x][pos] == 1:
                     return False
-                cols[y].add(val)                
+                rows[x][pos] = 1
+                
+                if cols[y][pos] == 1:
+                    return False
+                cols[y][pos] = 1
                 
                 idx = (x // 3) * 3 + y // 3
-                if val in subs[idx]: 
+                if subs[idx][pos] == 1:
                     return False
-                subs[idx].add(val)
+                subs[idx][pos] = 1
                 
         return True
                     
