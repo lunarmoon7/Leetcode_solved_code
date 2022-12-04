a5a31ab47275b4617412fee4777b114fc5d6f8b9
lunar moon <49crehbgr@gmail.com>
2022-12-04 18:12:11 +0900

    Time: 56 ms (71.29%), Space: 14 MB (30.06%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -5,11 +5,9 @@ class Solution:
         """
         n = len(matrix)
         
-        for i in range(n // 2 + n % 2):
-            for j in range(n // 2):
-                tmp = matrix[n - 1 - j][i]
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
-                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i]
-                matrix[j][n - 1 - i] = matrix[i][j]
-                matrix[i][j] = tmp
+        for i in range(n // 2):
+            matrix[i], matrix[n - 1 - i] = matrix[n - 1 - i], matrix[i]
         
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
