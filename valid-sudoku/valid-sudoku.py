5f90fb560cb84deaacd3ae311f34fcc53f0813a3
lunar moon <49crehbgr@gmail.com>
2022-12-04 17:53:34 +0900

    Time: 83 ms, Memory: 14 MB - LeetHub

--- valid-sudoku/valid-sudoku.py
+++ valid-sudoku/valid-sudoku.py
@@ -1,38 +1,29 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row = col = 9
+        n = 9
         
-        def checkRowCol(x, y):
-            # Check Row
-            num = board[x][y]
-            for i in range(9):
-                if i == y: continue
-                if board[x][i] == num:
-                    return False
-            # Check Col
-            for i in range(9):
-                if i == x: continue
-                if board[i][y] == num:
-                    return False
-            return True
+        rows = [[0] * n for _ in range(n)]
+        cols = [[0] * n for _ in range(n)]
+        subs = [[0] * n for _ in range(n)]
+        
+        for x in range(n):
+            for y in range(n):
                 
-        # Check 3*3 sub boxes
-        def checkSub(x, y):
-            q = x // 3
-            r = y // 3
-            check = [False] * 10
-            for i in range(q * 3, q * 3 + 3):
-                for j in range(r * 3, r * 3 + 3):
-                    if board[i][j] == '.': continue
-                    num = int(board[i][j])
-                    if check[num]:
-                        return False
-                    check[num] = True
-            return True
-        
-        for x in range(row):
-            for y in range(col):
-                if board[x][y] == '.': continue
-                if not checkRowCol(x, y) or not checkSub(x, y):
+                if board[x][y] == ".": continue
+                
+                pos = int(board[x][y]) - 1
+                
+                if rows[x][pos] == 1:
                     return False
+                rows[x][pos] = 1
+                
+                if cols[y][pos] == 1:
+                    return False
+                cols[y][pos] = 1
+                
+                idx = (x // 3) * 3 + y // 3
+                if subs[idx][pos] == 1:
+                    return False
+                subs[idx][pos] = 1
+                
         return True
