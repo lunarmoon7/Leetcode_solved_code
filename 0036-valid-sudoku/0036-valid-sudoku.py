a85f7243e491eaa1efabeddb75fa029513986e30
lunar moon <49crehbgr@gmail.com>
2022-12-04 17:44:43 +0900

    Time: 215 ms (52.54%), Space: 13.9 MB (35.36%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,40 +1,28 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row = col = 9
+        n = 9
         
-        def checkRowCol(x, y):
-            # Check Row
-            num = board[x][y]
-            for i in range(9):
-                if i == y: continue
-                if board[x][i] == num:
-                    return False
-            # Check Col
-            for i in range(9):
-                if i == x: continue
-                if board[i][y] == num:
-                    return False
-            return True
+        rows = [set() for _ in range(n)]
+        cols = [set() for _ in range(n)]
+        subs = [set() for _ in range(n)]
+        
+        for x in range(n):
+            for y in range(n):
+                val = board[x][y]
+                if val == ".": continue
                 
-        # Check 3*3 sub boxes
-        def checkSub(x, y):
-            q = x // 3
-            r = y // 3
-            check = [False] * 10
-            for i in range(q * 3, q * 3 + 3):
-                for j in range(r * 3, r * 3 + 3):
-                    if board[i][j] == '.': continue
-                    num = int(board[i][j])
-                    if check[num]:
-                        return False
-                    check[num] = True
-            return True
-        
-        for x in range(row):
-            for y in range(col):
-                if board[x][y] == '.': continue
-                if not checkRowCol(x, y) or not checkSub(x, y):
+                if val in rows[x]: 
                     return False
+                rows[x].add(val)
+                
+                if val in cols[y]: 
+                    return False
+                cols[y].add(val)                
+                
+                idx = (x // 3) * 3 + y // 3
+                if val in subs[idx]: 
+                    return False
+                subs[idx].add(val)
+                
         return True
-
-        
+                    
